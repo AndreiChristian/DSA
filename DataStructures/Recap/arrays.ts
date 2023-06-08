4074e0f95a4fa25c8d505303666d088385312b4b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-08 10:17:43 +0200

    >

---
+++ DataStructures/Recap/arrays.ts
@@ -0,0 +1,15 @@
+// Insertion order is kept
+
+// Element access via index
+
+// Iterable
+
+// Dynamic heigh
+
+// Duplicate values
+
+// Duplicate values are allowed
+
+// mixing types is allowed( in JS )
+
+console.log([1, 2, 3]);
