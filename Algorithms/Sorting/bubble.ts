6ff514ca106a615d014b380d0405b3b9a5930c5b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-11 10:56:16 +0200

    hello

--- Algorithms/Sorting/bubble.ts
+++ Algorithms/Sorting/bubble.ts
@@ -4,12 +4,14 @@ const random1 = createRandomArray(10);
 
 const bubbleSort = (nums: number[]): void => {
   console.log("Initial", ...nums);
+  let itCount = 0;
   for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] > nums[j]) {
-        [nums[i], nums[j]] = [nums[j], nums[i]];
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
       }
-      console.log(...nums);
+      itCount++;
+      console.log(`Iteration ${itCount}: `, ...nums);
     }
   }
   console.log("Final", ...nums);
