f119f8d47f6eff0d561ec6b2a4b2f84091a3b31f
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-26 17:31:45 +0200

    some js

--- Problems/620.sql
+++ Problems/620.sql
@@ -33,3 +33,8 @@
 -- +----+------------+-------------+--------+
 -- Explanation: 
 -- We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
+SELECT *
+FROM cinema
+WHERE description <> 'boring'
+    AND mod(id, 2) = 1
+ORDER BY rating DESC;
